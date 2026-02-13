7b3088ed2054b587a3ca6044bdcd132a4e51b648
Luis Antonio Marcelino <luisntoniomarcelinocolina@gmail.com>
2026-02-12 23:19:55 -0300

    Configuracoes inicial do projeto

---
+++ server.js
@@ -0,0 +1,7 @@
+import app from "./src/app.js";
+import "dotenv/config";
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor executando porta ${port}...`);
+});
